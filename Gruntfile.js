4079843340dffb71b08cf902d2307f9df9235e00
Patrick DOS SANTOS <copper.himself@gmail.com>
2015-01-03 17:46:15 +0100

    [update] changes to add some jasmine-requirejs for future tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,34 @@
 module.exports = function(grunt) {
+	'use strict';
+	
 	// Definition of constants
-	var ALL_INTERNAL_JS = "/**/*.js";
-	var JS_IN_SRC = "js" + ALL_INTERNAL_JS;
-	var JS_IN_TEST = "tests" + ALL_INTERNAL_JS;
+	var ALL_INTERNAL_JS = '/**/*.js';
+	var SRC = 'modules';
+	var JS_IN_SRC = SRC + ALL_INTERNAL_JS;
+	var JS_IN_TEST = 'tests' + ALL_INTERNAL_JS;
 
-	var ALL_JS_FILES = [ "Gruntfile.js", JS_IN_SRC, JS_IN_TEST ];
+	var ALL_JS_FILES = [ 'Gruntfile.js', JS_IN_SRC, JS_IN_TEST ];
 	
 	// Project configuration
 	grunt.initConfig( {
-		pkg: grunt.file.readJSON( "package.json" ),
+		pkg: grunt.file.readJSON( 'package.json' ),
 
 		jasmine: {
 			src: JS_IN_SRC,
 			options: {
-				specs: JS_IN_TEST
+				specs: JS_IN_TEST,
+				template: require( 'grunt-template-jasmine-requirejs' ),
+				templateOptions: {
+					requireConfig: {
+						baseUrl: SRC
+					}
+                }
 			},
 		},
 
 		jshint: {
 			options: {
-				jshintrc: ".jshintrc"
+				jshintrc: '.jshintrc'
 			},
 			all: ALL_JS_FILES
 		},
@@ -27,25 +36,25 @@ module.exports = function(grunt) {
 		watch: {
 			scripts: {
 				files: ALL_JS_FILES,
-				tasks: [ "jasmine" ]
+				tasks: [ 'jasmine' ]
 			}
 		},
 		
 		uglify: {
 			target: {
 				files: {
-					"pathfinder.min.js": [ JS_IN_SRC ]
+					'pathfinder.min.js': [ JS_IN_SRC ]
 				}
 			}
 		}
 	} );
 
 	// Load plugins
-	grunt.loadNpmTasks( "grunt-contrib-jshint" );
-	grunt.loadNpmTasks( "grunt-contrib-jasmine" );
-	grunt.loadNpmTasks( "grunt-contrib-watch" );
-	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jasmine' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 
 	// Default task
-	grunt.registerTask( "default", [ "jshint" ] );
+	grunt.registerTask( 'default', [ 'jshint' ] );
 };
